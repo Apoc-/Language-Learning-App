f20cec53c1339c46d578d28d275440f6b598221b
Shiang <pulorsok@gmail.com>
2019-02-15 14:59:12 +0800

    add TestingView

---
+++ Assets/Scripts/TestingView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ReturnButton()
+    {
+        Debug.Log("Close Testing View ");
+        Destroy(this.gameObject);
+    }
+}
