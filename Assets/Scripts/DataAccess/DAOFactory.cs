8dc2d95a02f2aebbf7509bc575aac2fff519c19a
René Ziegler <ziegler.rene87@gmx.de>
2019-02-21 16:55:54 +0100

    Refactored DAOFactory

--- Assets/Scripts/DataAccess/DAOFactory.cs
+++ Assets/Scripts/DataAccess/DAOFactory.cs
@@ -1,42 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataAccess
+﻿namespace DataAccess
 {
     class DAOFactory
     {
-        public static ILeitnerBoxDAO LeitnerBoxDAO
-        {
-            get { return new LeitnerBoxDAO(); }
-        }
-
-        public static IUserDAO UserDAO
-        {
-            get { return new UserDAO(); }
-        }
-
-        public static IAlphabetDAO AlphabetDAO
-        {
-            get { return new AlphabetDAO(); }
-        }
-
-        public static IVocabularyDAO VocabularyDAO
-        {
-            get { return new VocabularyDAO(); }
-        }
-
-        public static ISayingDAO SayingDAO
-        {
-            get { return new SayingDAO(); }
-        }
-
-        public static IDialogueDAO DialogueDAO
-        {
-            get { return new DialogueDAO(); }
-        }
-
+        public static ILeitnerBoxDAO LeitnerBoxDAO => new LeitnerBoxDAO();
+        public static IUserDAO UserDAO => new UserDAO();
+        public static IAlphabetDAO AlphabetDAO => new AlphabetDAO();
+        public static IVocabularyDAO VocabularyDAO => new VocabularyDAO();
+        public static ISayingDAO SayingDAO => new SayingDAO();
+        public static IDialogueDAO DialogueDAO => new DialogueDAO();
     }
 }
