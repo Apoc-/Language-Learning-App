2741b15a2634ab2abfb7fe397ca33d5cf808d4ea
René Ziegler <ziegler.rene87@gmx.de>
2019-02-23 12:19:16 +0100

    Added Prefab vor ModalDialogueCanvasBehaviour with first Dialogue (LevelUp)

---
+++ Assets/Scripts/UI/ModalDialogueCanvasBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UI
+{
+    public class ModalDialogueCanvasBehaviour : MonoBehaviour
+    {
+        public ModalDialogueBehaviour LevelUpDialogue;
+
+        private readonly List<ModalDialogueBehaviour> dialogues = new List<ModalDialogueBehaviour>();
+
+        private void Start()
+        {
+            dialogues.Add(LevelUpDialogue);
+        }
+
+        public void EnableLevelUpDialogue()
+        {
+            gameObject.SetActive(true);
+            DisableAllDialogues();
+
+            LevelUpDialogue.gameObject.SetActive(true);
+        }
+
+        public void DismissDialogue()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void DisableAllDialogues()
+        {
+            foreach (var dia in dialogues)
+            {
+                dia.gameObject.SetActive(false);
+            }
+        }
+    }
+}
