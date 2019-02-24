a61d3d9adfdf55730de59fdd54c4cc10554ce02a
René Ziegler <ziegler.rene87@gmx.de>
2019-02-24 16:05:06 +0100

    Added data for dialogues and access methods in DataCache
    Added missing categories
    Added caching for alphabet, sayings and dialogues

--- Assets/Scripts/DataProvider/DataCache.cs
+++ Assets/Scripts/DataProvider/DataCache.cs
@@ -10,44 +10,13 @@ namespace DataProvider
 {
     public class DataCache
     {
-        private Dictionary<string, Translation> _translations;
-        
         private DataCache()
         {
             LoadLocalizationData();
         }
         
-        private void LoadLocalizationData()
-        {
-            _translations = new Dictionary<string, Translation>();
-
-            var translationList = DAOFactory.TranslationDAO.LoadTranslations();
-            translationList.ForEach(translation => { _translations[translation.Key] = translation; });
-        }
-
-        public Alphabet GetAlphabet()
-        {
-            return DAOFactory.AlphabetDAO
-                .LoadAlphabet()
-                .Find(alphabet => alphabet.Type == GamificationManager.Instance.User.LearningLanguage);
-        }
-
-
-        public List<Saying> GetSayingsByCategory(string categoryId)
-        {
-            return DAOFactory.SayingDAO
-                .LoadSayings()
-                .Where(say => say.Category.Id == categoryId)
-                .ToList();
-        }
-
-        public List<Dialogue> GetDialoguesByCategory(string categoryId)
-        {
-            return DAOFactory.DialogueDAO
-                .LoadDialogues()
-                .Where(dia => dia.Category.Id == categoryId)
-                .ToList();
-        }
+        #region Translation
+        private Dictionary<string, Translation> _translations;
         
         public string GetUiTranslationByKey(string key)
         {
@@ -61,7 +30,80 @@ namespace DataProvider
                     throw new ArgumentOutOfRangeException();
             }
         }
+        
+        private void LoadLocalizationData()
+        {
+            _translations = new Dictionary<string, Translation>();
 
+            var translationList = DAOFactory.TranslationDAO.LoadTranslations();
+            translationList.ForEach(translation => { _translations[translation.Key] = translation; });
+        }
+        #endregion
+        
+        #region Alphabet
+        private Alphabet _alphabet;
+        public Alphabet GetAlphabet()
+        {
+            if (_alphabet == null)
+            {
+                _alphabet = DAOFactory.AlphabetDAO
+                    .LoadAlphabet()
+                    .Find(alphabet => alphabet.Type == GamificationManager.Instance.User.LearningLanguage);    
+            }
+
+            return _alphabet;
+        }
+        #endregion
+
+        #region Dialogues
+        //way to much hardcoded stuff, needs to be refactored in the future
+        private readonly Dictionary<string, List<Dialogue>> _dialogues = new Dictionary<string, List<Dialogue>>();
+        public List<Dialogue> GetDialoguesByCategory(string categoryId)
+        {
+            if (_dialogues.Count == 0) LoadDialogues();
+
+            return _dialogues[categoryId];
+        }
+
+        private void LoadDialogues()
+        {
+            var dialogues = DAOFactory.DialogueDAO.LoadDialogues();
+            var dialogueCategoryIds = new List<string>
+            {
+                "restaurant", "formal", "smalltalk", "shopping"
+            };
+            
+            dialogueCategoryIds.ForEach(id =>
+            {
+                _dialogues[id] = dialogues.Where(d => d.Category.Id == id).ToList();
+            });
+        }
+        #endregion
+        
+        #region Sayings
+        private readonly Dictionary<string, List<Saying>> _sayings = new Dictionary<string, List<Saying>>();
+        public List<Saying> GetSayingsByCategory(string categoryId)
+        {
+            if (_sayings.Count == 0) LoadSayings();
+
+            return _sayings[categoryId];
+        }
+
+        private void LoadSayings()
+        {
+            var sayings = DAOFactory.SayingDAO.LoadSayings();
+            var sayingCategoryIds = new List<string>
+            {
+                "birthday", "festivals", "weather", "behaviour"
+            };
+            
+            sayingCategoryIds.ForEach(id =>
+            {
+                _sayings[id] = sayings.Where(d => d.Category.Id == id).ToList();
+            });
+        }
+        #endregion
+        
         #region Vocabulary
         private List<Vocabulary> _vocabulary = new List<Vocabulary>();
         public List<Vocabulary> GetVocabulary()
