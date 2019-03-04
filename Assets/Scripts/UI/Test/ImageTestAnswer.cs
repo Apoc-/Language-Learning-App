457a597306d2baf58149f92f5022e43b69e89e8c
Matt <matthias.kleinhenz@yahoo.de>
2019-03-04 23:51:16 +0800

    Added ImageAnswer prefab

---
+++ Assets/Scripts/UI/Test/ImageTestAnswer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LeitnerSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageTestAnswer : TestAnswer
+{
+    public Image Image;
+
+    private LearnViewCanvas canvas;
+    private ImageAnswer answer;
+
+    public override void PopulateUI(LearnViewCanvas c, Answer a)
+    {
+        canvas = c;
+        answer = a.AsImageAnswer();
+
+        Image.sprite = answer.Image;
+    }
+
+    public void OnClick()
+    {
+        canvas.SelectAnswer(this);
+    }
+
+    public override Answer GetAnswer()
+    {
+        return answer;
+    }
+}
